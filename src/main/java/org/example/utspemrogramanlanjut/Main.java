b079e618f602481f0b6b9da922dc1aea2f29c569
rezadrian01 <ahmadadrian324@gmail.com>
2024-10-19 09:55:50 +0700

    refactor: move code in Main to App and use Main file to be entry point

--- src/main/java/org/example/utspemrogramanlanjut/Main.java
+++ src/main/java/org/example/utspemrogramanlanjut/Main.java
@@ -1,29 +1,8 @@
 package org.example.utspemrogramanlanjut;
 
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
 
-import java.io.IOException;
-
-public class Main extends Application {
-
-    @Override
-    public void start(Stage stage) throws IOException {
-        try{
-            Parent root = FXMLLoader.load(getClass().getResource("/org/example/utspemrogramanlanjut/fxml/Welcome.fxml"));
-            Scene scene = new Scene(root);
-            stage.setTitle("Event Management App");
-            stage.setScene(scene);
-            stage.show();
-        }catch(IOException e){
-            e.printStackTrace();
-        }
-    }
-
+public class Main {
     public static void main(String[] args) {
-        launch();
+        App.main(args);
     }
 }
